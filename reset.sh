8f8405e98ae2ef8372538488a1de425cf76ae00f
Jesse <jesse@piascik.net>
2017-08-27 09:45:46 -0600

    finish reset.js

--- reset.sh
+++
@@ -1,2 +0,0 @@
-git reset --hard 6310a3e446539dc3b3bc73ec42696f7830602577
-git push -f origin master
--- src/reset.js
+++ src/reset.js
@@ -1,6 +1,23 @@
 require('dotenv').config()
 const ghGot = require('gh-got')
 const token = process.env.TOKEN
+const shell = require('shelljs')
+
+function logExec (cmd, code, stdout, stderr) {
+  console.log(cmd)
+  console.log('--------------------------------------')
+  console.log('Exit code:', code)
+  console.log('Program output:', stdout)
+  console.log('Program stderr:', stderr)
+}
+
+function exec (name, cmd, cb) {
+  shell.exec(cmd, {silent:true}, (code, stdout, stderr) => {
+    logExec(cmd, code, stderr, stdout)
+    if (code !== 0) return cb(new Error(`${name} error: ${stderr}`))
+    cb(null, stdout)
+  })
+}
 
 async.series({
   deleteIssues: cb => {
@@ -29,6 +46,15 @@ async.series({
     }).catch(cb)
   },
   deleteBranch: cb => {
-
+    exec('delete remote branch', 'git push origin --delete imdone-todo-updates', cb)
+  },
+  resetToBaseVersion: cb => {
+    exec('reset to base version 1.0', 'git reset --hard 6310a3e446539dc3b3bc73ec42696f7830602577', cb)
+  },
+  pushToGitHub: cb => {
+    exec('push to github', 'git push -f origin master', cb)
   }
+}, (err, result) => {
+  if (err) return console.log(err)
+  console.log(result)
 })
