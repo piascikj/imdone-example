bbf4cb915d58928e928435d1c7ed32f1f73f346e
Jesse <jesse@piascik.net>
2017-11-20 13:07:53 -0700

    TODO comments updated by @imdoneio [ci skip] [imdone skip]

--- home/test.js
+++ home/test.js
@@ -1,3 +1,3 @@
-// TODO:20 a later task in TODO
-// FIXME:0 a fixme task
-// TODO:10 a task in TODO
+// TODO: 0 a later task in TODO id:2 gh:786
+// FIXME: a fixme task id:3 gh:787
+// TODO: 0 a task in TODO id:4 gh:788
