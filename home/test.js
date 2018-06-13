dd9404a10ec0ccf1b4144f08ae74c2abc0f1da49
Jesse Piascik <jesse@piascik.net>
2018-06-13 11:58:28 -0600

    Update test.js

--- home/test.js
+++ home/test.js
@@ -2,4 +2,4 @@
 // Jesse Piascik
 // jesse@piascik.net
 // https://github.com/piascikj/imdone-example/issues/1081
-// DOING: Fix issue creation and closure +feature
+// DOING: Fix issue creation and closure +feature +imdone
