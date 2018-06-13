d499d322d65e76a997e7f33502a137bc25d288a2
imdone.io app user <jesse@piascik.net>
2018-06-13 13:58:39 -0400

    TODO comments updated by @imdoneio [ci skip] [imdone skip]

--- home/test.js
+++ home/test.js
@@ -2,4 +2,7 @@
 // Jesse Piascik
 // jesse@piascik.net
 // https://github.com/piascikj/imdone-example/issues/1081
-// DOING: Fix issue creation and closure +feature +imdone
+// DOING: Fix issue creation and closure +feature +imdone id:8
+// Jesse Piascik
+// jesse@piascik.net
+// https://github.com/piascikj/imdone-example/issues/1089
