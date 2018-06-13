b5edb05848a997182ba05db4fffb0250406ad0d1
Jesse Piascik <jesse@piascik.net>
2018-06-13 14:38:19 -0600

    Update test.js

--- home/test.js
+++ home/test.js
@@ -2,7 +2,3 @@
 // Jesse Piascik
 // jesse@piascik.net
 // https://github.com/piascikj/imdone-example/issues/1147
-// DOING: some new stuff id:8
-// Jesse Piascik
-// jesse@piascik.net
-// https://github.com/piascikj/imdone-example/issues/1152
