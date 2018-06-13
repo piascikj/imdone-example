251866f78ca2957959d1643454015242684a53c9
Jesse Piascik <jesse@piascik.net>
2018-06-13 13:54:50 -0600

    Update test.js

--- home/test.js
+++ home/test.js
@@ -2,11 +2,3 @@
 // Jesse Piascik
 // jesse@piascik.net
 // https://github.com/piascikj/imdone-example/issues/1133
-// FIXME: a fixme task id:3
-// Jesse Piascik
-// jesse@piascik.net
-// https://github.com/piascikj/imdone-example/issues/1135
-// TODO: a task in TODO id:5
-// Jesse Piascik
-// jesse@piascik.net
-// https://github.com/piascikj/imdone-example/issues/1137
