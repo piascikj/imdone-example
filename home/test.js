a2422f51a02aa922cb287b2eb4c66d60a4cb41cf
imdone.io app user <jesse@piascik.net>
2018-06-13 14:07:56 -0400

    TODO comments updated by @imdoneio [ci skip] [imdone skip]

--- home/test.js
+++ home/test.js
@@ -1,3 +1,12 @@
-// TODO:20 a later task in TODO
-// FIXME:0 a fixme task
-// TODO:10 a task in TODO
+// TODO: a later task in TODO id:10
+// Jesse Piascik
+// jesse@piascik.net
+// https://github.com/piascikj/imdone-example/issues/1092
+// FIXME: a fixme task id:12
+// Jesse Piascik
+// jesse@piascik.net
+// https://github.com/piascikj/imdone-example/issues/1094
+// TODO: a task in TODO id:14
+// Jesse Piascik
+// jesse@piascik.net
+// https://github.com/piascikj/imdone-example/issues/1096
