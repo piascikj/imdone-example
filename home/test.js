43002ca2fc5a7125789148b565905fdb306f0d22
Jesse <jesse@piascik.net>
2018-06-14 09:34:36 -0600

    TODO comments updated by @imdoneio [ci skip] [imdone skip]

--- home/test.js
+++ home/test.js
@@ -2,4 +2,7 @@
 // Jesse Piascik
 // jesse@piascik.net
 // https://github.com/piascikj/imdone-example/issues/1180
-// DOING: Add a story +story
+// DOING: Add a story +story id:8
+// Jesse Piascik
+// jesse@piascik.net
+// https://github.com/piascikj/imdone-example/issues/1186
